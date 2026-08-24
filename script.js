317bf044cbe7e5f071961d07b5bb39023be3452d
Sachin11112006 <sachindfdc@gmail.com>
2026-08-24 22:30:25 +0530

    Fix indentation in key press event for Mario

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ document.addEventListener("keydown", function (e) {
 
   if (e.key === "d" || e.key === "ArrowRight") {
 
-    marioX = marioX + 10;
+     marioX = marioX + 10;
 
     if (marioX > 750) {
       marioX = 750;
